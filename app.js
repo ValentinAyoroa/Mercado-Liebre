338644a4d184392d04f67319148f918c5c0006c9
ValentinAyoroa <valentinayoroa@gmail.com>
2022-08-10 18:20:10 -0300

    MercadoLiebre terminado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(3000, () => {console.log('Servidor corriendo');});
 
 app.get('/', (req, res) => {

de07d952112b576e0a2025e45a422abb9d674fda
ValentinAyoroa <valentinayoroa@gmail.com>
2022-08-29 20:34:45 -0300

    Procfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {console.log('Servidor corriendo');});
+app.listen(process.env.PORT || 3000, () => {console.log('Servidor corriendo');});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
